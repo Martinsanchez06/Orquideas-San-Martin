3589f4b856aa571df12404106c01f3f14c0ba05b
Martinsanchez06 <sanchezmartin2435@gmail.com>
2022-10-05 07:47:53 -0500

    Middlewares funcionando

--- src/routes/client.js
+++ src/routes/client.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var router = express.Router();
 const ClientController = require('../Controllers/ClientController')
+const guestMiddleware = require('../middlewares/guestMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 // ------ OBTIENE VISTAS ------ 
 router.get('/listado', ClientController.lista);
 
 router.get('/detalle/:id', ClientController.detalle)
 
-router.get("/registro", ClientController.registroForm)
+router.get("/registro", guestMiddleware, ClientController.registroForm)
 
-router.get("/login", ClientController.loginForm)
+router.get("/login",  guestMiddleware, ClientController.loginForm)
 
-router.get("/perfil", ClientController.perfil)
+router.get("/perfil",authMiddleware , ClientController.perfil)
 
 router.get("/resultado", function(req, res){
 res.send('ok')
