6a094f2ba69eeca77916c404f4779d478705e943
Martinsanchez06 <sanchezmartin2435@gmail.com>
2022-11-24 11:35:21 -0500

    Errores

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -6,18 +6,18 @@ module.exports = {
     "host": "remotemysql.com",
     "dialect": "mysql"
   },
-  "test": {
-    "username": "sql8529233",
-    "password": "GSWYbmdnGM",
-    "database": "sql8529233",
-    "host": "sql8.freesqldatabase.com",
+  "development": {
+    "username": "5bPvv5pu0J",
+    "password": "0G4pdlFY1B",
+    "database": "5bPvv5pu0J",
+    "host": "remotemysql.com",
     "dialect": "mysql"
   },
-  "production": {
-    "username": "sql8529233",
-    "password": "GSWYbmdnGM",
-    "database": "sql8529233",
-    "host": "sql8.freesqldatabase.com",
+  "development": {
+    "username": "5bPvv5pu0J",
+    "password": "0G4pdlFY1B",
+    "database": "5bPvv5pu0J",
+    "host": "remotemysql.com",
     "dialect": "mysql"
-  }
+  },
 }
