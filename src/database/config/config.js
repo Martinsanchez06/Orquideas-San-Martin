339970cccf86a1b5723cef5b653a4950b3ee152b
Martinsanchez06 <sanchezmartin2435@gmail.com>
2022-11-24 12:01:01 -0500

    Errores

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -6,14 +6,14 @@ module.exports = {
     "host": "remotemysql.com",
     "dialect": "mysql"
   },
-  "development": {
+  "test": {
     "username": "5bPvv5pu0J",
     "password": "0G4pdlFY1B",
     "database": "5bPvv5pu0J",
     "host": "remotemysql.com",
     "dialect": "mysql"
   },
-  "development": {
+  "production": {
     "username": "5bPvv5pu0J",
     "password": "0G4pdlFY1B",
     "database": "5bPvv5pu0J",
